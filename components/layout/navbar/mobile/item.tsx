c632c0e4f4b32fa03184a12dd94fa281997b5954
Nam Tran <nama3k67lnq@gmail.com>
2025-03-18 12:10:28 +0700

    feat: simplify mobile navbar item click handling and improve link behavior

--- components/layout/navbar/mobile/item.tsx
+++ components/layout/navbar/mobile/item.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-
 interface MobileNavbarItemProps {
   href: string;
   children: React.ReactNode;
@@ -14,20 +12,13 @@ export default function MobileNavbarItem({
   href,
   setOpen,
 }: MobileNavbarItemProps) {
-  const router = useRouter();
-
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-    e.preventDefault();
+  const handleClick = () => {
     setOpen(false);
-    // Allow the dialog animation to complete before navigating
-    setTimeout(() => {
-      router.push(`#${href}`);
-    }, 100);
   };
 
   return (
     <li className="h-16 flex items-center justify-center text-sm uppercase font-bold not-last:border-b">
-      <Link onClick={handleClick} href={`#${href}`}>
+      <Link href={`#${href}`} onClick={handleClick}>
         {children}
       </Link>
     </li>
